51fc515d9f5c6f5657e71b9f2641313d6579c956
Veronica <javisores@gmail.com>
2024-04-16 12:54:54 +0200

    Frontend

--- LM/Diseño/Menuseleccion/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.menuseleccion {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.menuseleccion to javafx.fxml;
-    exports com.example.menuseleccion;
-}

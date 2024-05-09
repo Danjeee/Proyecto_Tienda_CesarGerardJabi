2bc6c36ba38d18d16f6377805ebd5dffc16e9a92
Veronica <javisores@gmail.com>
2024-05-09 13:59:10 +0200

    implementando

---
+++ Programacion/Pantallas/ProductView/src/main/java/tienda_javi_gerard_cesar/AdministrarEmpleadosController.java
@@ -0,0 +1,32 @@
+package tienda_javi_gerard_cesar;
+
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import tienda_javi_gerard_cesar.Clases.MenuHamb;
+
+public class AdministrarEmpleadosController {
+
+    @FXML
+    private AnchorPane cont;
+
+    @FXML
+    private App PantallaPrincipal = new App();
+
+    @FXML
+    public void retroceder_PanelAdmin(ActionEvent actionEvent) throws IOException {
+        App.setRoot("PanelAdministracion_Cesar_Javi_Gerard");
+    }
+
+    public void initialize() {
+        MenuHamb.popupHambMake();
+        cont.getChildren().add(MenuHamb.menuShadow);
+        cont.getChildren().add(MenuHamb.popupHamb);
+        cont.getChildren().add(MenuHamb.menuHamb());
+    }
+}

138484e7844c25bdbd7a85947ce58b28a99881da
cesar torres <151839354+ceesartorres@users.noreply.github.com>
2024-05-20 14:00:09 +0200

    .

--- Programacion/Pantallas/panel_admin/src/main/java/panel_admin/AltaProductosController.java
+++ Programacion/Pantallas/panel_admin/src/main/java/panel_admin/AltaProductosController.java
@@ -3,13 +3,8 @@ package panel_admin;
 import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.Statement;
 
-import panel_admin.ConexionSQL;
-import panel_admin.Clases.*;
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -21,15 +16,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.stage.FileChooser.ExtensionFilter;
-import panel_admin.Clases.Articulo;
 import panel_admin.MenuHamburguesa.MenuHamb;
 
 public class AltaProductosController {
@@ -39,19 +27,66 @@ public class AltaProductosController {
 
     @FXML
     private TextField nombre;
+
     @FXML
     private TextField precio;
+
     @FXML
     private TextField marca;
+
     @FXML
     private TextArea descripcion;
+
     @FXML
     private CheckBox activo;
+
     @FXML
     private TextField nombre_imagen;
+
+    @FXML
+    private TextField talla;
+
+    @FXML
+    private TextField color;
+
+    @FXML
+    private TextField tipo_manga;
+
+    @FXML
+    private TextField estilo;
+
+    @FXML
+    private TextField tipo_pantalon;
+
+    @FXML
+    private TextField tipo_suela;
+
+    @FXML
+    private TextField tipo_cierre;
+
+    @FXML
+    private TextField tien_bolsillos;
+
+    @FXML
+    private TextField capacidad;
+
+    @FXML
+    private CheckBox estampada;
+
+    @FXML
+    private CheckBox impermeable;
+
+    @FXML
+    private CheckBox personalizado;
+
+
     @FXML
     private ChoiceBox<String> material;
-    private String[] opciones = {"Algodón","Poliéster", "Seda", "Cuero", "Lana", "Pana", "Franela", "Lino", "Cachemira", "Terciopelo"};
+    private String[] opciones_material = {"Algodón","Poliéster", "Seda", "Cuero", "Lana", "Pana", "Franela", "Lino", "Cachemira", "Terciopelo"};
+
+    @FXML
+    private ChoiceBox<String> tipoArticulo;
+    private String[] opciones_articulo = {"Camisa","Chaqueta", "Pantalón", "Bolso", "Zapatos"};
 
     @FXML
     private Button imagen;
@@ -155,12 +190,72 @@ public class AltaProductosController {
 
     }
 
+    @FXML
+    private void opcionesArticulo() {
+
+        talla.setVisible(false);
+        color.setVisible(false);
+        tipo_cierre.setVisible(false);
+        impermeable.setVisible(false);
+        tipo_manga.setVisible(false);
+        estampada.setVisible(false);
+        tipo_pantalon.setVisible(false);
+        tien_bolsillos.setVisible(false);
+
+        estilo.setVisible(false);
+        personalizado.setVisible(false);
+        tipo_cierre.setVisible(false);
+        capacidad.setVisible(false);
+        tipo_suela.setVisible(false);
+        talla.setVisible(false);
+
+
+        if (tipoArticulo.getValue() == "Camisa" || tipoArticulo.getValue() == "Chaqueta" || tipoArticulo.getValue() == "Pantalón" ){
+            talla.setVisible(true);
+            color.setVisible(true);
+        
+        }else{
+            estilo.setVisible(true);
+            personalizado.setVisible(true);
+        }
+
+        switch (tipoArticulo.getValue()) {
+            case "Camisa":
+                tipo_manga.setVisible(true);
+                estampada.setVisible(true);
+                break;
+
+            case "Chaqueta":
+                impermeable.setVisible(true);
+                break;
+
+            case "Pantalón":
+                tipo_pantalon.setVisible(true);
+                tien_bolsillos.setVisible(true);
+                break;
+
+            case "Bolso":
+                tipo_cierre.setVisible(true);
+                capacidad.setVisible(true);
+                break;
+
+            case "Zapatos":
+                tipo_suela.setVisible(true);
+                talla.setVisible(true);
+                break;
+                
+            default:
+                break;
+        }
+    }
+
     public void initialize() {
         MenuHamb.popupHambMake();
         cont.getChildren().add(MenuHamb.menuShadow);
         cont.getChildren().add(MenuHamb.popupHamb);
         cont.getChildren().add(MenuHamb.menuHamb());
 
-        material.getItems().addAll(opciones);
+        material.getItems().addAll(opciones_material);
+        tipoArticulo.getItems().addAll(opciones_articulo);
     }
 }

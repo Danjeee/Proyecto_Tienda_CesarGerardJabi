6c899a6af5c62dbdfd96f944e436207fed41929b
Gerard Navarro <gerardnavarro36@gmail.com>
2024-05-20 14:01:53 +0200

    editarempleado funciona

---
+++ Programacion/Pantallas/login/src/main/java/login/Clases/MetodoPago.java
@@ -0,0 +1,44 @@
+package login.Clases;
+public class MetodoPago {
+    
+    private int codigo;
+    private String descripcion;
+
+    
+    public MetodoPago(int codigo, String descripcion) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public int hashCode() {
+            String nombre = String.valueOf(this.codigo);
+            String code = "";
+            for (int i = 0; i < nombre.length(); i++) {
+                code += Integer.valueOf(nombre.charAt(i));
+            }
+            return Integer.parseInt(code);
+
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        MetodoPago metodo = (MetodoPago) obj;
+        return codigo == metodo.codigo;
+    }
+    
+}
--- Programacion/Pantallas/login/src/main/java/login/EditarEmpleadoController.java
+++ Programacion/Pantallas/login/src/main/java/login/EditarEmpleadoController.java
@@ -1,23 +1,68 @@
 package login;
 
-
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import login.Clases.Alertas;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.CheckBox;
 
 public class EditarEmpleadoController {
 
-    public static String current;
+    @FXML
+    private App PantallaPrincipal = new App();
+
+    @FXML
+    public void cargarVentana_registro(ActionEvent actionEvent) throws IOException {
+        App.setRoot("Login");
+    }
+
+    @FXML
+    private void limpiarBotonE(ActionEvent event) {
+        nombreEmpleado.setText(null);
+    }
+
+    @FXML
+    private void limpiarBotonA(ActionEvent event) {
+        apellidosEmpleado.setText(null);
+    }
+
+    @FXML
+    private void limpiarBotonDNI(ActionEvent event) {
+        DNIempleado.setText(null);
+    }
+
+    @FXML
+    private void limpiarBotonDirecc(ActionEvent event) {
+        direccionEmpleado.setText(null);
+    }
+
+    @FXML
+    private void limpiarBotonFe(ActionEvent event) {
+        fechaNac.setValue(null);
+    }
+
+    @FXML
+    private void limpiarBotonEmai(ActionEvent event) {
+        emailEmpleado.setText(null);
+    }    
+
+    @FXML
+    private void limpiarBotonTelef(ActionEvent event) {
+        numtelef.setText(null);
+    }    
 
     @FXML
     private TextField nombreEmpleado;
@@ -26,21 +71,88 @@ public class EditarEmpleadoController {
     private TextField apellidosEmpleado;
 
     @FXML
+    private TextField DNIempleado;
+
+    @FXML
+    private TextField direccionEmpleado;
+
+    @FXML
     private DatePicker fechaNac;
+
+    @FXML
+    private TextField emailEmpleado;
+
+    @FXML
+    private ChoiceBox<String> dpto;
+
+    @FXML
+    private CheckBox privilegiosEmpleado;
 
     @FXML
     private TextField numtelef;
 
+    public static String current = "12345678A";
+
+    public void initialize() {
+
+        Connection con = conenct();
+
+        try {
+            ArrayList<Integer> dpt = new ArrayList<>();
+
+            Statement st0 = con.createStatement();
+            ResultSet rs0 = st0.executeQuery("SELECT * FROM departamento");
+
+            while (rs0.next()) {
+                dpto.getItems().add(rs0.getString("codigo") + ".- " + rs0.getString("nombre"));
+            }
+
+            nombreEmpleado.setText("Pepe");
+
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM empleado WHERE DNI = '" + current + "'");
+            
+
+            while (rs.next()) {
+                nombreEmpleado.setText(rs.getString("nombre"));
+                apellidosEmpleado.setText(rs.getString("apellidos"));
+                DNIempleado.setText(rs.getString("DNI"));
+                numtelef.setText(rs.getString("telefono"));
+                emailEmpleado.setText(rs.getString("email"));
+                fechaNac.setValue(LocalDate.parse(rs.getString("f_nacimiento")));
+                direccionEmpleado.setText(rs.getString("direccion"));
+                privilegiosEmpleado.selectedProperty().set(rs.getBoolean("tiene_privilegios"));
+                Statement st1 = con.createStatement();
+                ResultSet rs1 = st.executeQuery("SELECT * FROM departamento WHERE codigo = "+rs.getInt("dpto"));
+                while (rs1.next()) {
+                    dpto.getSelectionModel().select(rs1.getString("codigo")+".- "+rs1.getString("nombre"));
+                }
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    
+
     @FXML
-    private void editarEmpleado(){
+    private void editarEmpBoton(){
 
         Connection connection = conenct();
         Alert alerta = new Alert(Alert.AlertType.NONE);
-        
+
         try {
        
             Statement st = connection.createStatement();
             
+            st.executeUpdate("INSERT INTO CLIENTE VALUES('"+DNI.getText()+"','"+nombre.getText()+"','"+apellidos.getText()+"','"+telefono.getText()+"','"+fechanac.getValue()
+            +"','"+direccion.getText()+"','"+email.getText()+"','"+contraseña.getText()+"','0',' 0', 'Direccion',"+tarjetaFide.isSelected()+","+activo.isSelected()+",1)");
+            alerta.setAlertType(AlertType.INFORMATION);
+            alerta.setHeaderText(null);
+            alerta.setContentText("El usuario se ha registrado correctamente.");
+            alerta.show();
 
             connection.close();
         } catch (SQLException e) {
@@ -48,6 +160,30 @@ public class EditarEmpleadoController {
             e.printStackTrace();
             Alertas.errorRegistrar();
         }
+
+    }
+
+    @FXML
+    private void editarEmpleado() {
+
+        Connection connection = conenct();
+
+        try {
+            Statement st = connection.createStatement();
+            st.executeUpdate(
+                    update("departamento", String.valueOf(dpto.getSelectionModel().getSelectedItem().charAt(0))));
+
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            Alertas.errorRegistrar();
+
+        }
+    }
+
+    private String update(String field, String data) {
+        return "UPDATE empleado SET " + field + " = '" + data + "' WHERE DNI = '" + current + "'";
     }
 
     private Connection conenct() {
@@ -58,5 +194,5 @@ public class EditarEmpleadoController {
             e.printStackTrace();
         }
         return con;
-    }    
+    }
 }

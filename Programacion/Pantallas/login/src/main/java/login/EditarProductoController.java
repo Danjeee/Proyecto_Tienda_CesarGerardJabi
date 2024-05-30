824cb7f208b390301c816dab305be60480617e73
Gerard Navarro <gerardnavarro36@gmail.com>
2024-05-30 14:59:27 +0200

    intento de hacer el editar producto

--- Programacion/Pantallas/login/src/main/java/login/EditarProductoController.java
+++ Programacion/Pantallas/login/src/main/java/login/EditarProductoController.java
@@ -1,13 +1,11 @@
 package login;
 
 import java.io.File;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalDate;
 import java.util.ArrayList;
 
 import javafx.event.ActionEvent;
@@ -144,54 +142,49 @@ public class EditarProductoController {
     private ChoiceBox<String> tipoArticuloProd;
     private String[] opciones_articulo = { "Camisa", "Chaqueta", "Pantalón", "Bolso", "Zapatos" };
 
-    public static String current = "1";
+    public static int current = 5;
 
     /* */
+
+    private boolean esRopa() {
+        Connection con = conenct();
+        try {
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("SELECT cod_art FROM ropa");
+            while (rs.next()) {
+                if (rs.getInt("cod_art") == current) {
+                    return true;
+                }
+            }
+            return false;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+
+    }
 
     @FXML
     private void initialize() {
 
         Connection connection = conenct();
-/*
+
         int material_num = 0;
 
-        switch (materialProd.getValue()) {
-            case "Algodón":
-                material_num = 1;
-                break;
-            case "Poliéster":
-                material_num = 2;
-                break;
-            case "Seda":
-                material_num = 3;
-                break;
-            case "Cuero":
-                material_num = 4;
-                break;
-            case "Lana":
-                material_num = 5;
-                break;
-            case "Pana":
-                material_num = 6;
-                break;
-            case "Franela":
-                material_num = 7;
-                break;
-            case "Lino":
-                material_num = 8;
-                break;
-            case "Cachemira":
-                material_num = 9;
-                break;
-            case "Terciopelo":
-                material_num = 10;
-                break;
-        } */
+        tipoArticuloProd.setOnAction(e -> {
+            try {
+                Statement st69 = connection.createStatement();
+            ResultSet rs69 = st69.executeQuery("SELECT cod_art FROM ropa where nombre = '"+tipoArticuloProd.getSelectionModel().getSelectedItem()+"'");
+            current = rs69.getInt("cod_art");
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        });
 
         try {
 
-            /*ESTO ES PARA EL CHOICE BOX DE MATERIAL */
-            
+            /* ESTO ES PARA EL CHOICE BOX DE MATERIAL */
+
             ArrayList<Integer> material = new ArrayList<>();
             Statement st0 = connection.createStatement();
             ResultSet rs0 = st0.executeQuery("SELECT * FROM material");
@@ -201,48 +194,163 @@ public class EditarProductoController {
 
             /* */
 
-            /*ESTO ES PARA EL CHOICE BOX DE TIPO DE PRODUCTO */
-            
+            /* ESTO ES PARA EL CHOICE BOX DE TIPO DE PRODUCTO */
+
             ArrayList<Integer> tipoProd = new ArrayList<>();
             Statement st3 = connection.createStatement();
             ResultSet rs3 = st3.executeQuery("SELECT * FROM ropa");
             while (rs0.next()) {
-                tipoArticuloProd.getItems().add(rs0.getString("cod_art") + ".- " + rs0.getString("tipo_ropa"));
+                tipoArticuloProd.getItems().add(rs0.getString("cod_art") + ".- " + rs3.getString("tipo_ropa"));
             }
 
             /* */
 
             Statement st = connection.createStatement();
-            ResultSet rs = st.executeQuery("SELECT * FROM articulo WHERE cod_art = '" + current + "'");
+            String ropa = tipoArticuloProd.getSelectionModel().getSelectedItem();
+            if (ropa == null) {
+                ropa = "Camisa básica";
+            }
+            ResultSet rs = st.executeQuery("SELECT * FROM articulo WHERE nombre = '" + ropa + "'");
 
             while (rs.next()) {
-                while (rs.next()) {
 
-                    nombreProd.setText(rs.getString("nombre"));
-                    marcaProd.setText(rs.getString("marca"));
-                    descripcionProd.setText(rs.getString("descripcion"));
-                    precioProd.setText(rs.getString("precio"));
-                    imagenProd.setText(rs.getString("imagen"));
-                    activoProd.selectedProperty().set(rs.getBoolean("activo"));
+                nombreProd.setText(rs.getString("nombre"));
+                marcaProd.setText(rs.getString("marca"));
+                descripcionProd.setText(rs.getString("descripcion"));
+                precioProd.setText(rs.getString("precio"));
+                imagenProd.setText(rs.getString("imagen"));
+                nombre_imagenProd.setText(rs.getString("imagen"));
+                activoProd.selectedProperty().set(rs.getBoolean("activo"));
 
-                    Statement st1 = connection.createStatement();
-                    ResultSet rs1 = st1.executeQuery("SELECT * FROM material WHERE codigo = " + rs.getInt("materialProd"));
-                    while (rs1.next()) {
-                        materialProd.getSelectionModel().select(rs1.getString("codigo") + ".- " + rs1.getString("denominacion"));
-                    }
+                Statement st1 = connection.createStatement();
+                ResultSet rs1 = st1.executeQuery("SELECT * FROM material WHERE codigo = " + rs.getInt("material"));
+                while (rs1.next()) {
+                    materialProd.getSelectionModel()
+                            .select(rs1.getString("codigo") + ".- " + rs1.getString("denominacion"));
+                }
 
-                    Statement st4 = connection.createStatement();
-                    ResultSet rs4 = st4.executeQuery("SELECT * FROM ropa WHERE codigo = " + rs.getInt("tipoArticuloProd"));
-                    while (rs1.next()) {
-                        tipoArticuloProd.getSelectionModel().select(rs1.getString("codigo") + ".- " + rs4.getString("tipo_ropa"));
+                ArrayList<Integer> dpt = new ArrayList<>();
+
+                Statement st01 = connection.createStatement();
+                // ResultSet rs01 = st01.executeQuery("SELECT * FROM articulo where cod_art in (Select cod_art from ropa)");
+                ResultSet rs01 = st01.executeQuery("select * from articulo, ropa where articulo.cod_art = ropa.cod_art");
+                while (rs01.next()) {
+                    tipoArticuloProd.getItems().add(rs01.getString("nombre"));
+                }
+            }
+
+            /* */
+
+            if (esRopa()) {
+                Statement st22 = connection.createStatement();
+                ResultSet rs22 = st22.executeQuery("SELECT * FROM ropa WHERE cod_art = " + current);
+                while (rs22.next()) {
+                    switch (rs22.getString("tipo_ropa")) {
+                        case "Camisa":
+                            tallaProd.setText(rs22.getString("talla"));
+                            colorProd.setText(rs22.getString("color"));
+                            tipo_cierreProd.setText(rs22.getString("tipo_cierre"));
+                            tipo_mangaProd.setText(rs22.getString("tipo_manga"));
+                            estampadaProd.setText(rs22.getString("estampada"));
+                            break;
+                        case "Chaqueta":
+                            tallaProd.setText(rs22.getString("talla"));
+                            colorProd.setText(rs22.getString("color"));
+                            tipo_cierreProd.setText(rs22.getString("tipo_cierre"));
+                            impermeableProd.selectedProperty().set(rs22.getBoolean("impermeable"));
+                            break;
+                        case "Pantalón":
+                            tallaProd.setText(rs22.getString("talla"));
+                            colorProd.setText(rs22.getString("color"));
+                            tipo_cierreProd.setText(rs22.getString("tipo_cierre"));
+                            tien_bolsillosProd.selectedProperty().set(rs22.getBoolean("tien_bolsillos"));
+                            tipo_pantalonProd.setText(rs22.getString("tipo_pantalon"));
+                            break;
+                        default:
+                            break;
                     }
                 }
+            } else {
+                Statement st22 = connection.createStatement();
+                ResultSet rs22 = st22.executeQuery("SELECT * FROM accesorio WHERE cod_art = " + current);
+                while (rs22.next()) {
+                    switch (rs22.getString("tipo_accesorio")) {
+                        case "Bolso":
+                            estiloProd.setText(rs22.getString("estilo"));
+                            personalizadoProd.selectedProperty().set(rs22.getBoolean("personalizado"));
+                            tipo_cierreProd.setText(rs22.getString("tipo_cierre"));
+                            capacidadProd.setText(rs22.getString("capacidad"));
+                            break;
+                        case "Zapatos":
+                            estiloProd.setText(rs22.getString("estilo"));
+                            personalizadoProd.selectedProperty().set(rs22.getBoolean("personalizado"));
+                            tipo_suelaProd.setText(rs22.getString("tipo_suela"));
+                            tallaProd.setText(rs22.getString("talla"));
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            switch (materialProd.getSelectionModel().getSelectedItem()) {
+                case "Algodón":
+                    material_num = 1;
+                    break;
+                case "Poliéster":
+                    material_num = 2;
+                    break;
+                case "Seda":
+                    material_num = 3;
+                    break;
+                case "Cuero":
+                    material_num = 4;
+                    break;
+                case "Lana":
+                    material_num = 5;
+                    break;
+                case "Pana":
+                    material_num = 6;
+                    break;
+                case "Franela":
+                    material_num = 7;
+                    break;
+                case "Lino":
+                    material_num = 8;
+                    break;
+                case "Cachemira":
+                    material_num = 9;
+                    break;
+                case "Terciopelo":
+                    material_num = 10;
+                    break;
             }
 
         } catch (SQLException ex) {
             ex.printStackTrace();
             Alertas.error("ERROR AL MOSTRAR EL PRODUCTO");
         }
+
+        /* Ropa */
+        vbox_talla.setVisible(false);
+        vbox_color.setVisible(false);
+        vbox_tipoCierre.setVisible(false);
+        vbox_impermeable.setVisible(false);
+        vbox_tipoManga.setVisible(false);
+        vbox_estampada.setVisible(false);
+        vbox_tipoPantalon.setVisible(false);
+        vbox_tieneBolsillo.setVisible(false);
+
+        /* Accesorios */
+        vbox_estilo.setVisible(false);
+        vbox_personalizado.setVisible(false);
+        vbox_tipoCierre.setVisible(false);
+        vbox_capacidad.setVisible(false);
+        vbox_tipoSuela.setVisible(false);
+        vbox_tallaZapatos.setVisible(false);
+
+        tipoArticuloProd.setOnAction(e -> opcionesArticulo());
+
     }
 
     public void abrirImagen(ActionEvent e) {
@@ -265,6 +373,65 @@ public class EditarProductoController {
         imageView.setFitHeight(200);
 
         imagenProd.setGraphic(imageView);
+
+    }
+
+    @FXML
+    private void opcionesArticulo() {
+
+        /* Ropa */
+        vbox_talla.setVisible(false);
+        vbox_color.setVisible(false);
+        vbox_tipoCierre.setVisible(false);
+        vbox_impermeable.setVisible(false);
+        vbox_tipoManga.setVisible(false);
+        vbox_estampada.setVisible(false);
+        vbox_tipoPantalon.setVisible(false);
+        vbox_tieneBolsillo.setVisible(false);
+
+        /* Accesorios */
+        vbox_estilo.setVisible(false);
+        vbox_personalizado.setVisible(false);
+        vbox_tipoCierre.setVisible(false);
+        vbox_capacidad.setVisible(false);
+        vbox_tipoSuela.setVisible(false);
+        vbox_tallaZapatos.setVisible(false);
+
+        if (tipoArticuloProd.getValue() == "Camisa" || tipoArticuloProd.getValue() == "Chaqueta"
+                || tipoArticuloProd.getValue() == "Pantalón") {
+            vbox_talla.setVisible(true);
+            vbox_color.setVisible(true);
+            vbox_tipoCierre.setVisible(true);
+        } else {
+            vbox_estilo.setVisible(true);
+            vbox_personalizado.setVisible(true);
+        }
+
+        switch (tipoArticuloProd.getValue()) {
+            case "Camisa":
+                vbox_tipoManga.setVisible(true);
+                vbox_estampada.setVisible(true);
+                break;
+
+            case "Chaqueta":
+                vbox_impermeable.setVisible(true);
+                break;
+
+            case "Pantalón":
+                vbox_tipoPantalon.setVisible(true);
+                vbox_tieneBolsillo.setVisible(true);
+                break;
+
+            case "Bolso":
+                vbox_capacidad.setVisible(true);
+                vbox_tipoCierre.setVisible(true);
+                break;
+
+            case "Zapatos":
+                vbox_tipoSuela.setVisible(true);
+                vbox_tallaZapatos.setVisible(true);
+                break;
+        }
 
     }
 
